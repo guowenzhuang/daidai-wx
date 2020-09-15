62217c40d830e89b7f7703aa2b923d7a02278fac
guowenzhuang <80795289@qq.com>
2020-09-15 19:07:10 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    motto: 'Hi 开发者！',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    pageCur:'home',
+    addIconWidth:0,
+    addIconWhitePadding: 3
+  },
+  navChange(e){
+
+    this.setData({
+      pageCur: e.currentTarget.dataset.cur
+    })
+    console.log(this.data.pageCur)
+
+    if(this.data.pageCur == 'demand'){
+
+      wx.navigateTo({
+        url: '/pages/addDemand/home/home'
+      })
+    }
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+   
+    var that=this
+    var query = wx.createSelectorQuery();
+    query.select('.add-icon').boundingClientRect(function (rect) {
+      that.setData({
+        addIconWidth: rect.width
+      })
+    }).exec();
+
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
